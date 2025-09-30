78ccaa3de2d5b0afef48f1d5f0d8fc237693e411
Cameron Hermens <cameron.hermens@onxmaps.com>
2025-09-30 13:13:50 -0700

    Add distinct Map and List views on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { Map, List, ItemDetail, Donate } from './pages';
-import { Navigation, WelcomeModal } from './components';
+import { Navigation, WelcomeModal, MobileFooter } from './components';
 import { Unauthenticated } from 'convex/react';
 
-function App() {
+function AppContent() {
+  const location = useLocation();
   const [showWelcomeModal, setShowWelcomeModal] = useState(false);
 
+  const isMainView = location.pathname === '/' || location.pathname === '/list' || location.pathname === '/wingman' || location.pathname === '/wingman/';
+  
   useEffect(() => {
     // Check if user has seen the welcome modal in the last 24 hours
     const lastSeenTimestamp = localStorage.getItem('wingman-welcome-last-seen');
@@ -22,28 +25,36 @@ function App() {
     setShowWelcomeModal(false);
     localStorage.setItem('wingman-welcome-last-seen', Date.now().toString());
   };
+
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      <Navigation />
+      <main className={`flex-1 min-h-0 relative ${isMainView ? 'mobile-footer-padding' : ''}`}>
+        <Routes>
+          <Route path="/" element={<Map />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/item/:id" element={<ItemDetail />} />
+          <Route path="/donate" element={<Donate />} />
+          {/* Catch-all route for unknown paths */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
+      <MobileFooter />
+      <Unauthenticated>
+        <WelcomeModal 
+          isOpen={showWelcomeModal} 
+          onClose={handleCloseWelcome}
+        />
+      </Unauthenticated>
+    </div>
+  );
+}
+
+function App() {
   return (
     <Router basename="/wingman">
-        <div className="min-h-screen bg-gray-50 flex flex-col">
-          <Navigation />
-          <main className="flex-1 min-h-0 relative">
-            <Routes>
-              <Route path="/" element={<Map />} />
-              <Route path="/list" element={<List />} />
-              <Route path="/item/:id" element={<ItemDetail />} />
-              <Route path="/donate" element={<Donate />} />
-              {/* Catch-all route for unknown paths */}
-              <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-          </main>
-          <Unauthenticated>
-            <WelcomeModal 
-              isOpen={showWelcomeModal} 
-              onClose={handleCloseWelcome}
-            />
-          </Unauthenticated>
-        </div>
-      </Router>
+      <AppContent />
+    </Router>
   );
 }
 
